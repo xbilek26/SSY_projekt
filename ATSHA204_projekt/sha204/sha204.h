a015bf4ec2ddc3133d3b3ca9b16532141116a7c6
František Bílek <115495243+xbilek26@users.noreply.github.com>
2024-03-26 19:44:14 +0100

    Update sha204.h

--- ATSHA204_projekt/sha204/sha204.h
+++ ATSHA204_projekt/sha204/sha204.h
@@ -11,15 +11,15 @@
 
 // DEFINES
 
-#define SHA204_PACKET_FUNC_RESET     ((uint8_t) 0x00)
-#define SHA204_PACKET_FUNC_SLEEP     ((uint8_t) 0x01)
-#define SHA204_PACKET_FUNC_IDLE      ((uint8_t) 0x02)
-#define SHA204_PACKET_FUNC_CMD       ((uint8_t) 0x03)
+#define SHA204_PACKET_FUNC_RESET  ((uint8_t) 0x00)
+#define SHA204_PACKET_FUNC_SLEEP  ((uint8_t) 0x01)
+#define SHA204_PACKET_FUNC_IDLE  ((uint8_t) 0x02)
+#define SHA204_PACKET_FUNC_CMD  ((uint8_t) 0x03)
 
-#define I2C_FUNCTION_RETCODE_SUCCESS     ((uint8_t) 0x00) // communication with device succeeded
-#define I2C_FUNCTION_RETCODE_COMM_FAIL   ((uint8_t) 0xF0) // communication with device failed
-#define I2C_FUNCTION_RETCODE_TIMEOUT     ((uint8_t) 0xF1) // communication timed out
-#define I2C_FUNCTION_RETCODE_NACK        ((uint8_t) 0xF8) // TWI NACK
+#define I2C_FUNCTION_RETCODE_SUCCESS  ((uint8_t) 0x00) // communication with device succeeded
+#define I2C_FUNCTION_RETCODE_COMM_FAIL  ((uint8_t) 0xF0) // communication with device failed
+#define I2C_FUNCTION_RETCODE_TIMEOUT  ((uint8_t) 0xF1) // communication timed out
+#define I2C_FUNCTION_RETCODE_NACK  ((uint8_t) 0xF8) // TWI NACK
 
 
 #endif /* SHA204_H_ */
